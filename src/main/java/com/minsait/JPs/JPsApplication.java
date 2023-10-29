8fb35f1f4353909c4d105a0aee18773e1412076e
Danilosrr <danilo_srr@hotmail.com>
2023-10-29 12:53:11 -0300

    init spring

---
+++ src/main/java/com/minsait/JPs/JPsApplication.java
@@ -0,0 +1,13 @@
+package com.minsait.JPs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minsait/JPs/JPsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsait.JPs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
